79e4d6dc210162895211f2543647456cabc82bcc
Oleg Demakov <odemakov@gmail.com>
2025-01-02 07:39:57 +0100

    responsive login screen; disable login button it empty email & password; some linting

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,32 +1,33 @@
 <script setup lang="ts">
+import { onMounted } from "vue";
+import router from "@/router";
+import { useAuthStore } from "@/stores/AuthStore";
+import UserIcon from "./UserIcon.vue";
 
-import { onMounted } from 'vue';
-import router from '@/router';
-import { useAuthStore } from '@/stores/AuthStore';
-import UserIcon from './UserIcon.vue';
-
-const auth = useAuthStore()
+const auth = useAuthStore();
 
 onMounted(() => {
-  auth.fetchUser()
-})
+  auth.fetchUser();
+});
 
 const handleLogout = async () => {
-  await auth.logout()
-  router.push({ name: 'home' })
-}
-
+  auth.logout();
+  router.push({ name: "home" });
+};
 </script>
 
 <template>
   <div class="navbar bg-neutral-content">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl bg-slate-200" href="/">ScriptFlow</a>
+      <a class="btn btn-ghost text-xl bg-slate-200" href="./">ScriptFlow</a>
     </div>
     <div v-if="auth.isAuthenticated" class="flex-none gap-2">
       <div class="dropdown dropdown-end">
         <UserIcon :username="auth.user?.username" />
-        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        >
           <li><a>Settings</a></li>
           <li><a @click="handleLogout">Logout</a></li>
         </ul>
--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,42 +1,67 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
 
-import { useAuthStore } from '../stores/AuthStore';
-import Button from './Button.vue';
+import { useAuthStore } from "../stores/AuthStore";
+import Button from "./Button.vue";
 
-const auth = useAuthStore()
-const router = useRouter()
-const email = ref('')
-const password = ref('')
-const errorMessage = ref('')
+const auth = useAuthStore();
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+const errorMessage = ref("");
+const loginButtonDisabled = computed(() => !email.value || !password.value);
 
 const handleLogin = async () => {
-  errorMessage.value = ''
+  errorMessage.value = "";
   try {
-    await auth.login(email.value, password.value)
-    router.push({ name: 'home' })
+    await auth.login(email.value, password.value);
+    router.push({ name: "home" });
   } catch (error) {
-    errorMessage.value = (error as any).message
+    errorMessage.value = (error as any).message;
   }
-}
+};
 </script>
 
 <template>
-  <div class="mx-auto p-8 min-w-[400px] max-w-[400px] rounded shadow-lg bg-neutral-content">
+  <div
+    class="mx-auto p-4 sm:p-8 w-full max-w-[400px] rounded shadow-lg bg-neutral-content"
+  >
     <h2 class="text-xl font-bold">Login</h2>
     <div class="mt-4">
       <label for="email" class="block">Email:</label>
-      <input v-model="email" type="email" id="email" required placeholder="Enter your email"
-        class="w-full px-4 py-2 border rounded mt-2" />
+      <input
+        v-model="email"
+        type="email"
+        id="email"
+        required
+        placeholder="Enter your email"
+        class="w-full px-4 py-2 border rounded mt-2"
+      />
     </div>
     <div class="mt-4">
       <label for="password" class="block">Password:</label>
-      <input v-model="password" type="password" id="password" required placeholder="Enter your password"
-        class="w-full px-4 py-2 border rounded mt-2" />
+      <input
+        v-model="password"
+        type="password"
+        id="password"
+        required
+        placeholder="Enter your password"
+        class="w-full px-4 py-2 border rounded mt-2"
+      />
     </div>
     <div class="mt-4 text-center">
-      <Button @click="handleLogin" title="Login" />
+      <button
+        @click="handleLogin"
+        :disabled="loginButtonDisabled"
+        :class="[
+          'text-white px-4 py-2 rounded-md',
+          loginButtonDisabled ? 'bg-gray-400' : 'bg-gray-700',
+          { 'hover:bg-gray-600': !loginButtonDisabled },
+        ]"
+      >
+        Login
+      </button>
     </div>
     <p v-if="errorMessage" class="text-error mt-4">{{ errorMessage }}</p>
   </div>
